74f59b983aa7d86024c4e9bef775f5136cdc3a21
XueMei Lin <alu0101225845@ull.edu.es>
2021-05-02 23:55:39 +0100

    practica09

--- src/modificacion/ejercicio-2.ts
+++ src/modificacion/ejercicio-2.ts
@@ -1,10 +1,12 @@
-import {watchFile} from 'fs';
+import * as fs from 'fs';
 import {spawn} from 'child_process';
 
-
-watchFile('/home/usuario/practica09/ficheros', (curr, prev) => {
-
-    const wc = spawn('wc', ['']);
+/**
+ * Acceder al directorio correspondiente y mostrar las informacion sobre dicho fichero
+ */
+const filename: string = '/home/usuario/practica09/ficheros/test1.txt';
+fs.access(filename, fs.constants.R_OK, (err) => {
+    const wc = spawn('wc', ['-1', filename]);
 
     let wcOutput = '';
     wc.stdout.on('data', (piece) => wcOutput += piece);
--- src/modificacion/ejercicio-3.ts
+++ src/modificacion/ejercicio-3.ts
@@ -2,20 +2,3 @@
 
 import {watchFile} from 'fs';
 import {spawn} from 'child_process';
-
-
-watchFile('/home/usuario/practica09/ficheros', (curr, prev) => {
-
-    const wc = spawn('wc', ['']);
-
-    let wcOutput = '';
-    wc.stdout.on('data', (piece) => wcOutput += piece);
-
-    wc.on('close', () => {
-    const wcOutputAsArray = wcOutput.split(/\s+/);
-    console.log(`File has ${wcOutputAsArray[1]} lines`);
-    console.log(`File has ${wcOutputAsArray[2]} words`);
-    console.log(`File has ${wcOutputAsArray[3]} characters`);
-    
-  });
-});
