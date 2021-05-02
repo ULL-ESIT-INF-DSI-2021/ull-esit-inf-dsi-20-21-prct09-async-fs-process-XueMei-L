4f68a0fe7a4c4d3a090ffc096e38116b393ce4d7
XueMei-L <72436762+XueMei-L@users.noreply.github.com>
2021-05-03 00:36:17 +0200

    Delete modificacion directory

--- modificacion/ejercicio-1.ts
+++
@@ -1,8 +0,0 @@
-const directory = './home/usuario/practica09/ficheros';
-const fs: any = require('fs');
-
-fs.readdir(directory, (err:any, files:any) => {
-    files.forEach(file => {
-        console.log(file);
-    });
-});
